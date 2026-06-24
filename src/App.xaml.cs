2b236f863df3f83014cdd0d1f075e8bc6f8ac407
haitodo <aquasar_info@yahoo.co.jp>
2026-06-24 09:55:07 +0900

    アプリ終了時にwindows live captionを強制終了

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
 
 using LiveCaptionsTranslator.utils;
@@ -8,7 +11,11 @@ namespace LiveCaptionsTranslator
     {
         App()
         {
+            // ① アプリケーションの正規の終了イベントを登録（より安全なタイミングで実行されます）
+            this.Exit += OnAppExit;
+            // ② 強制終了などのためのプロセス終了イベントも念のため残す
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
             Translator.Setting?.Save();
 
             Task.Run(() => Translator.SyncLoop());
@@ -25,12 +32,34 @@ namespace LiveCaptionsTranslator
             LocalizationManager.SetLanguage(lang);
         }
 
+        private void OnAppExit(object sender, ExitEventArgs e)
+        {
+            ForceKillLiveCaptions();
+        }
+
         private static void OnProcessExit(object sender, EventArgs e)
         {
-            if (Translator.Window != null)
+            ForceKillLiveCaptions();
+        }
+
+        // ③ ウィンドウ情報に依存せず、プロセス名で直接検索して確実に終了させる
+        private static void ForceKillLiveCaptions()
+        {
+            try
             {
-                LiveCaptionsHandler.RestoreLiveCaptions(Translator.Window);
-                LiveCaptionsHandler.KillLiveCaptions(Translator.Window);
+                var processes = Process.GetProcessesByName("LiveCaptions");
+                foreach (var process in processes)
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                        process.WaitForExit();
+                    }
+                }
+            }
+            catch
+            {
+                // アプリ終了時の例外（権限エラーや既に終了済みなど）は無視して安全に閉じる
             }
         }
     }
